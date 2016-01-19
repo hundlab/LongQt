c0cc072732e74aba4a2a2a527ef14725d899e3e6
Danielle Beckley <beckley.52@buckeyemail.osu.edu>
2016-01-19 13:14:05 -0500

    allow multiple plots on one graph
    allow multiple plots on one graph

--- dialog.cpp
+++ dialog.cpp
@@ -4,6 +4,7 @@
 
 #include <QIODevice>
 #include <QList>
+#include <QGridLayout>
 
 Dialog::Dialog(QWidget *parent) :
     QDialog(parent),
@@ -11,13 +12,21 @@ Dialog::Dialog(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    ui->plot->addGraph();
+    ui->plot->graph(0)->setPen(QPen(Qt::blue));
+    ui->plot->graph(0)->setBrush(QBrush(QColor(0, 0, 255, 20))); // first graph will be filled with translucent blue
+    ui->plot->addGraph();
+    ui->plot->graph(1)->setPen(QPen(Qt::red));
+
+
     int size = 6000;                            //may vary- Dani Watch
-    QVector<double> x(size), y(size);
+    QVector<double> x(size), y(size), y0(size);
     int low_x_axis = 0;
     int max_x_axis = 0;
 
+
     /**
-     * Gettiing data from simulation for graphing
+     * Gettiing data from simulation for graphing for 1st Plot
      */
     QFile file("./data/dtestdocy.txt");
     if(!file.open(QIODevice::ReadOnly)){
@@ -31,12 +40,13 @@ Dialog::Dialog(QWidget *parent) :
         QStringList split_line = line.split("\t");
         x[point] = split_line[1].toDouble();
         y[point] = split_line[2].toDouble();
+        y0[point] = split_line[2].toDouble() + 10;  //because I don't have different inputs, changed it by 10
         point++;
     }
     file.close();
 
     /**
-     *  Getting data from Simvar file for xaxis information
+     * Setting  boundaries of x axis
      */
     QFile simfile("./simvars.txt");
     if(!simfile.open(QIODevice::ReadOnly)){
@@ -55,8 +65,8 @@ Dialog::Dialog(QWidget *parent) :
     }
     simfile.close();
 
-    ui->plot->addGraph();
-    ui->plot->graph(0)->setData(x,y);
+    ui->plot->graph(0)->setData(x, y);
+    ui->plot->graph(1)->setData(x, y0);
     ui->plot->xAxis->grid()->setVisible(false);
     ui->plot->yAxis->grid()->setVisible(false);
     ui->plot->xAxis->setLabelFont(QFont(font().family(), 16));
@@ -65,8 +75,8 @@ Dialog::Dialog(QWidget *parent) :
     ui->plot->yAxis->setLabel("Voltage (mv)");
     ui->plot->xAxis->setRange(low_x_axis, max_x_axis);
     ui->plot->yAxis->setRange(-60, 40);             //brittle-need to pull in details from file
+    ui->plot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
 }
-
 Dialog::~Dialog()
 {
     delete ui;
