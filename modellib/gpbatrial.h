9c1b11bc1853884c7f884ea75b8ca39963d0d92a
Daniel Gratz <gratz.dhg@gmail.com>
2016-04-06 13:11:55 -0400

    added atriol modle source code

---
+++ modellib/gpbatrial.h
@@ -0,0 +1,178 @@
+// 10/29/2012
+//################################################
+// This header file contains all class definitions
+//#################################################
+#include "cell.h" //parent class definitions
+
+#ifndef GPBATRIAL_H
+#define GPBATRIAL_H
+
+/*########################*/
+/*    DEFINE STRUCTS	  */
+/*########################*/
+struct GateVariable {
+   double m;
+   double h;
+   double j;
+   double xkr;
+   double xks;
+   double xkur;
+   double ykur;
+   double xf;
+   double yf;
+   double f_cabj;
+   double f_cabsl;
+   double d;	
+   double f;	
+};	
+//####################################
+// subclass of Cell
+//####################################
+class GpbAtrial: public Cell
+{
+  public:
+    // constructors
+    GpbAtrial();
+    virtual ~GpbAtrial();
+
+    double Vsl;
+    double Vjunc;
+    double Vsr;
+	
+//###### Concentrations #########
+    double naO;
+    double kO;
+    double najI;
+    double naslI;
+    double cajI;
+    double caslI;
+    double caO;
+    double caI;
+    double kI;
+    double caSr;
+    double clO;
+    double clI;
+    double naI;
+    double mgI;
+//####### RyR channel fractions
+    double Ryrr;
+    double Ryro;
+    double Ryri;
+    double RI;
+//###### Currents #######
+    double iNajunc;
+    double iNasl;
+    double iNa;
+    double iNabjunc;
+	double iNabsl;
+	double iNab;
+	double iNaKjunc;
+	double iNaKsl;
+	double iNak;
+	double iKr;
+	double iKs; 
+	double iKsjunc;
+	double iKssl;
+	double iKpjunc;
+	double iKpsl;
+	double iKp;
+	double iTos;
+	double iTof;
+	double iTo;
+	double iK1;
+	double iKur;
+	double iClcajunc;
+	double iClcasl;
+	double iClca;
+	double iClbk;
+	double iCajunc;
+	double iCasl;
+	double iCa;
+	double iCak;
+	double iCanajunc;
+	double iCanasl;
+	double iCana;
+	double iCaL;
+	double iCatotjunc;
+	double iCatotsl;
+	double iNcxjunc;
+	double iNcxsl;
+	double iNaca;
+	double ipCajunc;
+	double ipCasl;
+	double ipCa;
+	double iCabjunc;
+	double iCabsl;
+	double iCab;
+	double iNatotjunc;
+	double iNatotsl;
+	double iClt;
+	
+//##### Buffers ########
+	//Sodium Buffers
+	double Nabj; 
+	double Nabsl; 
+	
+	//Calcium Buffers
+	double TnCl;
+	double TnChc;
+	double TnChm;
+	double CaM;
+	double Myoc;
+	double Myom;
+	double SRB;
+	
+	//junctional and SL Ca buffers
+	double SLLj;
+	double SLLsl;
+	double SLHj;
+	double SLHsl;
+	
+	//SR Ca buffer
+	double Csqnb;
+	double dCsqnb;
+
+//##### Fluxes ########    
+	double Jsrcarel;
+	double Jserca;
+	double Jsrleak;
+	double J_cabcyto;
+	double J_Cabjunc; 
+	double J_Cabsl;
+
+//##### Fractional Currents ########
+
+	double Fjunc;
+	double Fsl;
+	double F_juncCaL;
+	double F_slCaL;
+
+ //###Concentration updating functions ######
+	virtual void updatecaI();
+	virtual void updatenaI();
+ //####Current updating functions #######
+	/*virtual void updateSRcurrents();*/
+	virtual void updateIcal();
+	virtual void updateIcab();
+	virtual void updateIpca();
+	virtual void updateIto();
+	virtual void updateIks();
+	virtual void updateIkr();
+	virtual void updateIk1();
+	virtual void updateIkur();
+	virtual void updateIpk();
+	virtual void updateInaca();
+	virtual void updateInak();
+	virtual void updateInab();
+	virtual void updateIna();
+	virtual void updateSRFlux();
+	virtual void updatecytobuff(); //cytosolic Ca buffers
+	virtual void updateJSLbuff(); //junctional and SL Ca buffers
+	virtual void updateSRbuff(); //SR Ca buffer	 
+	virtual void updateIclca();
+	virtual void updateIclbk();
+        virtual int stim();
+    virtual map<string, double*> makemap();
+    struct GateVariable gate;
+};
+#endif
